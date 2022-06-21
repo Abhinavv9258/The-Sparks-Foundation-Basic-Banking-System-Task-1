398a3e2be02791b99b96181ca1b4c7a8bc774fbc
Abhinavv9258 <abhinavv218@gmail.com>
2022-06-21 23:22:29 +0530

    WriteCommit

---
+++ connection/connection.php
@@ -0,0 +1,13 @@
+<?php
+$server_name = "localhost";
+$uname = "root";
+// $pass = "Fuzzi@#4567";
+// $pass = "";
+$database_name = "sparks_bank";
+$conn = mysqli_connect($server_name, $uname);
+//now check the connection
+if (!$conn) {
+die("Connection Failed:" . mysqli_connect_error());
+}
+mysqli_select_db($conn, $database_name);
+?>
